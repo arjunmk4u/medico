e361e4dbe4f2349f5f35ff3beb5473978f762ddc
arjunmk4u <arjunmk4u@gmail.com>
2025-06-05 10:19:56 +0530

    moved to localhost

--- src/components/PredictionTool.jsx
+++ src/components/PredictionTool.jsx
@@ -3,8 +3,7 @@ import axios from "axios";
 import { motion } from "framer-motion";
 
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "http://localhost:5001";
 
 const MLPredictor = ({ isOpen, onClose, type }) => {
   const [formData, setFormData] = useState({
